f7aa7445ee21ac430a6c2adff0e4db5e00307920
wojciechkochmanski124 <wojciechkochmanski58@gmail.com>
2025-10-26 18:57:01 +0000

    add solution for zadanie2_2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
         } catch (Exception e) {
             System.err.println("Błąd podczas uruchamiania Programu 1: " + e.getMessage());
         }
+        // 2. Wywołanie Zadanie2_2
+        System.out.println("\n[Wywołuję Program2.main...]");
+        try {
+            // Z przekazywaniem argumentów (np. z argumentów przekazanych do Main)
+            String[] args2 = {"parametr1", "parametr2"}; 
+            Zadanie2_2.main(args2); 
+
+            // Opcjonalnie: możesz przekazać argumenty, które otrzymała klasa Main:
+            // Program2.main(args); 
+
+        } catch (Exception e) {
+            System.err.println("Błąd podczas uruchamiania Programu 2: " + e.getMessage());
+        }
     }
 }
---
+++ src/Zadanie2_2.java
@@ -0,0 +1,6 @@
+public class Zadanie2_2 {
+    public static void main(String[] args) {
+        int liczba = 10;    
+        System.out.println("Wartość zmiennej liczba: " + liczba);
+    }
+}
